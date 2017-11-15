e9225ce33dc22bfc8d8d332ace7111325d984618
HaiUIT <haiuit9795@gmail.com>
2017-11-15 21:49:52 +0700

    Initial commit

---
+++ src/main/java/com/pricecompare/PricecompareApplication.java
@@ -0,0 +1,14 @@
+package com.pricecompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricecompareApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PricecompareApplication.class, args);
+	}
+}
